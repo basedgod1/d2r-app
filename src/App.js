58bbd4804a634720b19c3195866016b214953b01
pcargile <preston.cargile@gmail.com>
2022-01-02 14:36:08 +0000

    Add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <br/><Button>React Bootstrap</Button>
       </header>
     </div>
   );
