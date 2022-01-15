e3a6ba30326e26c0eb10c65d663895edbeceefa4
pcargile <preston.cargile@gmail.com>
2022-01-15 05:56:26 +0000

    Filter potions

---
+++ filter.mjs
@@ -0,0 +1,57 @@
+import { readFile, writeFile } from 'fs/promises';
+
+const readExcel = async (file) => {
+  let items = {};
+  let data = await readFile(file, 'utf8');
+  let lines = data.split(/\r?\n/);
+  let keys = lines.shift().split(/\t/);
+  lines.forEach((line) => {
+    var item = {};
+    let values = line.split(/\t/);
+    values.forEach((value, index) => {
+      item[keys[index]] = value;
+    });
+    if (item.code) {
+      items[item.code] = item;
+    }
+  });
+  return items;
+};
+
+const readJson = async (file) => {
+  let data = await readFile(file, 'utf8');
+  return JSON.parse(data);
+};
+
+const armor = await readExcel('data/armor.txt');
+const misc = await readExcel('data/misc.txt');
+const weapons = await readExcel('data/weapons.txt');
+const items = { ...armor, ...misc, ...weapons };
+const types = await readExcel('data/itemtypes.txt');
+const filters = await readJson('data/item-names.json');
+
+const potions = (filter, item, type) => {
+  if (!item.type) {
+    return filter;
+  }
+  if (/^(a|s|w)pot$/.test(item.type) || /^(hp|mp)[1-4]$/.test(item.code)) {
+    filter.enUS = '';
+  }
+  if (item.code == 'hp5') {
+    filter.enUS = 'HP';
+  }
+  if (item.code == 'mp5') {
+    filter.enUS = 'MP';
+  }
+  return filter;
+};
+
+filters.forEach((filter, index) => {
+  let item = items[filter.Key];
+  if (!item) {
+    return;
+  }
+  filters[index] = potions(filter, item, types[item.type]);
+});
+
+await writeFile('data/filter.json', JSON.stringify(filters, null, 2).replace(/\n/g, '\r\n') + '\r\n', 'utf8');
