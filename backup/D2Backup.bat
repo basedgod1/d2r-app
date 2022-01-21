c96e39ee9739124a730b9255950244254aa9b8c4
pcargile <preston.cargile@gmail.com>
2022-01-21 22:03:40 +0000

    Backup gomule locally

--- backup/D2Backup.bat
+++ backup/D2Backup.bat
@@ -7,6 +7,7 @@ set "TS=%DATE:~-4%%DATE:~-10,2%%DATE:~-7,2%%TIME:~-11,2%%TIME:~-8,2%%TIME:~-5,2%
 set "DEST=Diablo II Backups\d2bak%TS%\"
 mkdir "%SRC%%DEST%"
 xcopy "%SRC%%D2DIR%" "%SRC%%DEST%%D2DIR%" /v /i /s /e /h
+xcopy "%SRC%%GMDIR%" "%SRC%%DEST%%GMDIR%" /v /i /s /e /h /EXCLUDE:%SRC%%GMDIR%\exclude.txt
 mkdir "E:\%DEST%"
 xcopy "%SRC%%D2DIR%" "E:\%DEST%%D2DIR%" /v /i /s /e /h
 xcopy "%SRC%%GMDIR%" "E:\%DEST%%GMDIR%" /v /i /s /e /h /EXCLUDE:%SRC%%GMDIR%\exclude.txt
